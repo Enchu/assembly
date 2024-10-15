7857d63882f5d02ab6f321b2089c0f950a6b8469
Alexey <aleck.germanov@yandex.ru>
2024-10-15 11:20:12 +0300

    add migrate-mongo, radix-ui, eslint, prettier

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -1,36 +1,37 @@
-// In this file you can configure migrate-mongo
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
 
 const config = {
-  mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb://localhost:27017",
+	mongodb: {
+		// TODO Change (or review) the url to your MongoDB:
+		url: process.env.NEXT_PUBLIC_DB_URL,
 
-    // TODO Change this to your database name:
-    databaseName: "YOURDATABASENAME",
+		// TODO Change this to your database name:
+		databaseName: process.env.NEXT_PUBLIC_DB_NAME,
 
-    options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true, // removes a deprecating warning when connecting
-      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
-      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
-    }
-  },
+		options: {
+			useNewUrlParser: true, // removes a deprecation warning when connecting
+			useUnifiedTopology: true, // removes a deprecating warning when connecting
+			//   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+			//   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+		},
+	},
 
-  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: "migrations",
+	// The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+	migrationsDir: 'migrations',
 
-  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
-  changelogCollectionName: "changelog",
+	// The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+	changelogCollectionName: 'changelog',
 
-  // The file extension to create migrations and search for in migration dir 
-  migrationFileExtension: ".js",
+	// The file extension to create migrations and search for in migration dir
+	migrationFileExtension: '.js',
 
-  // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determine
-  // if the file should be run.  Requires that scripts are coded to be run multiple times.
-  useFileHash: false,
+	// Enable the algorithm to create a checksum of the file contents and use that in the comparison to determine
+	// if the file should be run.  Requires that scripts are coded to be run multiple times.
+	useFileHash: false,
 
-  // Don't change this, unless you know what you're doing
-  moduleSystem: 'commonjs',
+	// Don't change this, unless you know what you're doing
+	moduleSystem: 'commonjs',
 };
 
 module.exports = config;
